078496e900b963ee3c230699817ec2de325715e6
MY\yousei takei <yousei_san@icloud.com>
2020-09-14 20:01:40 +0900

    add build

--- docs/index.tsx
+++ docs/index.tsx
@@ -1,35 +1,33 @@
 import React, {FC, useState} from 'react';
 import ReactDOM from 'react-dom';
-import { Head, Icon, Card, Pills, Sides, Trans } from '@tsei/core'
-import { unregister } from './serviceWorker';
-import * as Examples from './examples'
-import { useGrid } from '../src'
-//import Mdmd from '@tsei/mdmd';
+import {useGrid} from '../src';
+import {unregister} from './serviceWorker';
+import {Head,Card,Notes,Sides,Trans} from '@tsei/core';
+import * as Examples from './examples';
+//import {useGesture} from 'react-use-gesture';
+//import {useSpring, animated as a} from 'react-spring';
+//import {Mdmd} from '@tsei/mdmd';
 
-import './styles.css'
+import './styles.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 
 const App :FC = () => {
-    /* state */
     const [lang, setLang] = useState<string>(window?.navigator?.language||'ja')
-    const [dark, setDark] = useGrid<boolean>({md:true, lg:false})
-    const [size, setSize] = useGrid<number> ({md:1   , lg:1.5 })
-    const style = {display:"flex",justifyContent:"center",alignItems:"center",margin:"auto"}
+    const [dark, setDark] = useGrid<number>({md:1, lg:0})
+    const [size, setSize] = useGrid<number>({md:1, lg:1.5 })
     return (
-        <div style={{background:dark?"#000":"#fff",minHeight:"100%",padding:size*100}}>
+        <div style={{minHeight:"100%",padding:size*100,
+                    transition:"1s",background:dark?"#000":"#f1f1f1",}}>
             <Head {...{size,dark}}>Examples</Head>
-            {/*<Notes {...{size,dark}}>*/}
-            {Object.entries(Examples).map(([key,Example])=>
-                <React.Fragment key={key}>
-                    <Head size={size/2}>{key}</Head>
-                    <Card {...{dark,style}}>
-                        <Example/>
-                    </Card>
-                </React.Fragment>
-            )}
-            {/*</Notes>*/}
+            {/* TODO initOrder to order */}
+            <Notes {...{size:1}} initOrder={Object.keys(Examples).map((_,i)=>i)}>
+                {Object.entries(Examples).map(([key,Example])=>
+                    <Card key={key}>
+                        <Example />
+                    </Card>)}
+            </Notes>
             <Sides {...{size}}>
                 <p onClick={()=>window.location.href="/note"}>Note</p>
                 <p onClick={()=>window.location.href="/hook"}>Hook</p>
@@ -40,13 +38,6 @@ const App :FC = () => {
                 <div onClick={()=>setDark((p:any)=>({md:p.lg,lg:p.md}))}>{dark?'🌛':'🌞'}</div>
                 <div onClick={()=>setSize((p:any)=>({md:p.lg,lg:p.md}))}>{size<75?'👶':'👨'}</div>
             </Trans>
-            <Pills {...{size}}>
-                <Icon   {...{dark,size}} fa="ellipsis-h"    onOpen={()=>null}>
-                  <Icon {...{dark,size}} fa="share-square"  onOpen={()=>null}/>
-                  <Icon {...{dark,size}} fa="sign-in-alt"   onOpen={()=>null}/>
-                  <Icon {...{dark,size}} fa="location-arrow"onOpen={()=>null}/>
-                </Icon>
-            </Pills>
         </div>
     )
 }
